b8b7b4759f4e2040d8a2102cf0c4cdb543a8205c
Dawn <dawn_passerini@harvard.edu>
2018-10-12 15:45:34 -0400

    Initial commit

---
+++ mysocialhub.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: My Social Hub
+Plugin URI: http://dawnp.sgedu.site
+Description: A plugin that pushes posts to Facebook, Instagram and Twitter
+Version: 1.0.0
+Author: Dawn Passerini
+Author URI: http://dawnp.sgedu.site
+License: GPL2
+*/
+
+//code starts here
